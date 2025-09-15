a8f2e4fd9e41c56441e9ef6952b903cc2deaa409
ktyler-aerospike <ktyler@aerospike.com>
2025-09-15 16:37:05 -0700

    Delete setup directory

--- setup/start_web.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-[ -s ${NVM_DIR}/nvm.sh ] && \. ${NVM_DIR}/nvm.sh &&
-[ -s ${NVM_DIR}/bash_completion ] && \. ${NVM_DIR}/bash_completion &&
-cd $HOME/projects/website &&
-nohup yarn preview --host 0.0.0.0 --port 8080 &
